f87b235bcfbca7ac3002432d7f3df046bd911142
Papiexx <boutdepapier27@outlook.fr>
2022-02-04 17:35:53 +0100

    register userfollows for administration panel

--- litreview/review/admin.py
+++ litreview/review/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 
-from review.models import Ticket, Review
+from review.models import ticket, review, userfollows
 
 
 class TicketAdmin(admin.ModelAdmin):
     """For administration panel"""
-    list_display = ('title', 'description', 'image', 'user')
+    list_display = ('title', 'description', 'image', 'user', 'is_archived')
 
 class ReviewAdmin(admin.ModelAdmin):
     """For administration panel"""
-    list_display = ('headline', 'rating', 'ticket', 'user')
+    list_display = ('ticket', 'headline', 'rating', 'user','get_is_archived')
 
-admin.site.register(Ticket, TicketAdmin)
-admin.site.register(Review, ReviewAdmin)
+    def get_is_archived(self, obj):
+        return obj.ticket.is_archived
+
+    get_is_archived.short_description = 'is_archived'
+    get_is_archived.admin_order_field = 'is_archived'
+
+class UserFollowsAdmin(admin.ModelAdmin):
+    """For administration panel"""
+    list_display = ['user', 'followed_user']
+
+admin.site.register(ticket.Ticket, TicketAdmin)
+admin.site.register(review.Review, ReviewAdmin)
+admin.site.register(userfollows.UserFollows, UserFollowsAdmin)
