33fee43c4f5ecf4dcfb636cec82820a2a5d246b3
Papiexx <boutdepapier27@outlook.fr>
2022-02-04 17:17:50 +0100

    minor changes

--- litreview/authentication/forms.py
+++ litreview/authentication/forms.py
@@ -5,8 +5,8 @@ from django import forms
 
 class LoginForm(forms.Form):
     """Login form"""
-    username = forms.CharField(max_length=63, label="Nom d'Utilisateur")
-    password = forms.CharField(max_length=63, widget=forms.PasswordInput, label="Mot de passe")
+    username = forms.CharField(max_length=63, widget=forms.TextInput(attrs={'style':'max-width: 24em'}), label="Nom d'Utilisateur")
+    password = forms.CharField(max_length=63, widget=forms.PasswordInput(attrs={'style':'max-width: 24em'}), label="Mot de passe")
 
 class SignupForm(UserCreationForm):
     """Registration form"""
