bf25a69dd2c00d52a34d0e9b12cb0acbf6d95308
Papiexx <boutdepapier27@outlook.fr>
2022-02-26 08:54:03 +0100

    adding radioselect

--- litreview/review/forms/review_form.py
+++ litreview/review/forms/review_form.py
@@ -1,12 +1,25 @@
+from tkinter.messagebox import NO
 from django import forms
 from ..models.review import Review
 
 class ReviewForm(forms.ModelForm):
     """Review Form"""
+    NOTES = (
+        ('0', '0'),
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+        ('4', '4'),
+        ('5', '5'),
+    )
+    rating = forms.ChoiceField(widget=forms.RadioSelect(), choices=NOTES)
+
+
     class Meta:
         model = Review
         fields = ['headline','rating', 'body']
         labels = {'headline': 'Titre', 'rating': 'Note', 'body': 'Commentaire'}
+
 
 class DeleteReviewForm(forms.Form):
     """Delete ticket form"""
--- litreview/review/models/review.py
+++ litreview/review/models/review.py
@@ -1,21 +1,14 @@
 from random import choices
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.conf import settings
 from . import ticket
 
 
 class Review(models.Model):
 
-    STATUS = (
-        ('&#9733;&#9734;&#9734;&#9734;&#9734;', ('1')),
-        ('&#9733;&#9733;&#9734;&#9734;&#9734;', ('2')),
-        ('&#9733;&#9733;&#9733;&#9734;&#9734;', ('3')),
-        ('&#9733;&#9733;&#9733;&#9733;&#9734;', ('4')),
-        ('&#9733;&#9733;&#9733;&#9733;&#9733;', ('5')),
-    )
-
     ticket = models.ForeignKey(to=ticket.Ticket, on_delete=models.CASCADE)
-    rating = models.CharField(choices=STATUS, max_length=128, default=1)
+    rating = models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
     headline = models.CharField(max_length=128)
     body = models.TextField(max_length=8192, blank=True)
     user = models.ForeignKey(
